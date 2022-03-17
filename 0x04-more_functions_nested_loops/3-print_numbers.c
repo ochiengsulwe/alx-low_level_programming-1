b28b9b3c7eef2bb635f9c13a1f605ca83eb7eb81
NdutaCharity <58165250+NdutaCharity@users.noreply.github.com>
2022-03-17 13:33:47 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - a function that prints the numbers, from 0-9
+ * print_numbers - a function that prints the numbers, from 0 to 9
  *
  * Return: 0-9 followed by newline
  */
@@ -10,6 +10,6 @@ void print_numbers(void)
 	int i;
 
 	for (i = 0; i <= 9; i++)
-		_putchar(i + 0);
+		_putchar(i + '0');
 	_putchar('\n');
 }
