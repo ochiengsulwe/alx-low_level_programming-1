2414a8b8e6fa58f997ca280be55763ec4a026806
NdutaCharity <58165250+NdutaCharity@users.noreply.github.com>
2022-03-17 14:13:56 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,8 +2,7 @@
 
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
- * _putchar only 3 times
- *followed by new line
+ * followed by a new line
  */
 void more_numbers(void)
 {
@@ -11,11 +10,11 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 15; j++)
+		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-				_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
