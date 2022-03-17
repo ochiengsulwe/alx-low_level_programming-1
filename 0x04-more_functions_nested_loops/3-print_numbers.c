689350bfc3d7fb496fc61f672fcf63602466720a
NdutaCharity <58165250+NdutaCharity@users.noreply.github.com>
2022-03-17 13:30:24 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,12 @@
  *
  * Return: 0-9 followed by newline
  */
+
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
-		putchar(i + '0');
-	putchar('\n');
+		_putchar(i + '0');
+	_putchar('\n');
 }
