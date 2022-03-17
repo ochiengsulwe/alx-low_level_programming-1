62fe9d36cfe67bf132368984e9e18f8fd21000eb
NdutaCharity <58165250+NdutaCharity@users.noreply.github.com>
2022-03-17 16:35:07 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 #include "main.h"
-
 /**
  * mul -  checks for checks for a digit (0 through 9).
  * @a: a -  Variable
@@ -15,21 +13,3 @@ int mul(int a, int b)
 
 	return (mul);
 }
-=======
-#include "main.h"
-
-/**
- * mul -  checks for checks for a digit (0 through 9)
- * @a: a -  Variable
- * @b: b - variable
- * Return: Always 0.
- */
-int mul(int a, int b)
-{
-	int mul;
-	
-	mul = a * b;
-
-	return (mul);
-}
->>>>>>> 80eb8c95faa8281dcf1e4bf4d676c17334d347e6
