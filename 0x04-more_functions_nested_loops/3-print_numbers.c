bba9b8d52778a2011c31b4e2ce63cc626e8a5007
NdutaCharity <58165250+NdutaCharity@users.noreply.github.com>
2022-03-17 14:59:08 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - a function that prints the numbers, from 0 to 9
+ * print_numbers - prints the numbers, from 0 to 9
  *
  * Return: 0-9 followed by newline
  */
