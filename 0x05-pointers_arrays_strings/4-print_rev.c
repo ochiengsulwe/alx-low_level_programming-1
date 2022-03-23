f8603238b265c108214aa724d0f81465b562d886
NdutaCharity <58165250+NdutaCharity@users.noreply.github.com>
2022-03-23 22:52:57 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,7 +8,6 @@ void print_rev(char *s)
 {
 	int longi = 0;
 	int o;
-
 	while (*s != '\0')
 	{
 		longi++;
@@ -20,6 +19,5 @@ void print_rev(char *s)
 		_putchar(*s);
 		s--;
 	}
-
 	_putchar('\n');
 }
