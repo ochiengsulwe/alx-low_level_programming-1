aeba0a6a974afee45d8dac80e7d2d91da2bb2fb6
NdutaCharity <58165250+NdutaCharity@users.noreply.github.com>
2022-03-23 22:49:44 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,18 @@
 #include "main.h"
 /**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: string to print
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
-void _puts(char *str)
+int _strlen(char *s)
 {
-	while (*str != '\0')
+	int longi = 0;
+
+	while (*s != '\0')
 	{
-		_putchar(*str++);
+		longi++;
+		s++;
 	}
-	_putchar('\n');
+
+	return (longi);
 }
