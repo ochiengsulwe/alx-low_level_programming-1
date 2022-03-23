33c4c76e32cd05732c998dcc6bd3cf81b48ec056
NdutaCharity <ndutacharityn@gmail.com>
2022-03-23 07:37:59 -0700

    pointer commits

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,8 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9
  *
- * Return: 0-9 followed by newline
+ * Return: 0 through 9 followed by newline
+ * Remove trailing white space characters from string
  */
 void print_numbers(void)
 {
