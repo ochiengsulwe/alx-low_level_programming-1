d26ce52a73419499b912043881599329e7aac36d
NdutaCharity <58165250+NdutaCharity@users.noreply.github.com>
2022-03-28 23:14:57 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,12 +11,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-        unsigned int i = 0;
-
+	unsigned int i = 0;
+	
 	while (i < n)
 	{
-		    *(s + i) = b;
-		      i += 1;
+		*(s + i) = b;
+		i += 1;
 	}
 	return (s);
 }
