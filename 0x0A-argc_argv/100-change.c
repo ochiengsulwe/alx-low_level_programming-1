41d9db24179b44850f9b94e277d01fc6e521e47b
NdutaCharity <58165250+NdutaCharity@users.noreply.github.com>
2022-04-04 21:40:14 +0300

    minimum number

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,68 +1,51 @@
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - prints the min num of coins to make change for an amount of money
- *
- * @argc: argument count
- * @argv: argument vector
+ * main - check the code
+ * Description - prints minimmum number of coin
+ * @argc: argument used
+ * @argv: argument used
  * Return: int
  */
 int main(int argc, char *argv[])
 {
-	unsigned int count = 0;
+	int total, count;
+	unsigned int i;
+	char *p;
+	int cents[] = {25, 10, 5, 2};
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else if (atoi(argv[1]) < 0)
+
+	total = strtol(argv[1], &p, 10);
+	count = 0;
+
+	if (!*p)
 	{
-		printf("%d\n", 0);
-		return (0);
+		while (total > 1)
+		{
+			for (i = 0; i < sizeof(cents[i]); i++)
+			{
+				if (total >= cents[i])
+				{
+					count += total / cents[i];
+					total = total % cents[i];
+				}
+			}
+		}
+		if (total == 1)
+			count++;
 	}
-	count = coin_count(count, atoi(argv[1]));
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 	printf("%d\n", count);
 	return (0);
-}
-
-/**
- * coin_count - counts the min coins needed for change
- *
- * @count: int to count coins
- * @num: argv[1] changed to int
- * Return: int
- */
-unsigned int coin_count(unsigned int count, int num)
-{
-	unsigned int sum = 0;
-
-	while (!(sum + 25 > (unsigned int)num))
-	{
-		sum += 25;
-		count++;
-	}
-	while (!(sum + 10 > (unsigned int)num))
-	{
-		sum += 10;
-		count++;
-	}
-	while (!(sum + 5 > (unsigned int)num))
-	{
-		sum += 5;
-		count++;
-	}
-	while (!(sum + 2 > (unsigned int)num))
-	{
-		sum += 2;
-		count++;
-	}
-	while (!(sum + 1 > (unsigned int)num))
-	{
-		sum += 1;
-		count++;
-	}
-	return (count);
 }
