35bbcdc737d55a423d71594078934b7a47e60ab5
NdutaCharity <58165250+NdutaCharity@users.noreply.github.com>
2022-04-08 23:58:28 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,42 @@
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * *str_concat - concatenates two strings
- * @s1: string to concatenate
- * @s2: other string to concatenate
- *
- * Return: pointer to the new string created (Success), or NULL (Error)
+ * str_concat - function that concatenates two strings.
+ * @s1: input char 1
+ * @s2: input char 2
+ * Return: char
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	char *cat;
+	char *nul = "";
+	unsigned int i, j, x, y;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
-
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
+	i = j = x = y = 0;
+	if (s1 == NULL)
+		s1 = nul;
+	if (s2 == NULL)
+		s2 = nul;
+	while (s1[i] != '\0')
+		i += 1;
+	while (s2[j] != '\0')
+		j += 1;
+	j += 1;
+	cat = malloc((i + j) * sizeof(*cat));
+	if (cat == NULL)
 		return (NULL);
-
-	i = 0;
-	j = 0;
-
-	if (s1)
+	while (s1[x] != '\0')
 	{
-		while (i < len1)
-		{
-			s3[i] = s1[i];
-			i++;
-		}
+		cat[x] = s1[x];
+		x += 1;
 	}
-
-	if (s2)
+	while (s2[y] != '\0')
 	{
-		while (i < (len1 + len2))
-		{
-			s3[i] = s2[j];
-			i++;
-			j++;
-		}
+		cat[x] = s2[y];
+		y += 1;
+		x += 1;
 	}
-	s3[i] = '\0';
-
-	return (s3);
+	cat[x] = '\0';
+	return (cat);
 }
